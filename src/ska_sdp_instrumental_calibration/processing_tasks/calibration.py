8385011eaa220f0c1d72455d04223d192e97b5ae
Daniel Mitchell <daniel.mitchell@csiro.au>
2024-11-04 16:45:53 +1100

    YAN-1884: moved solve_gaintable call to function solve_bandpass

--- src/ska_sdp_instrumental_calibration/processing_tasks/calibration.py
+++ src/ska_sdp_instrumental_calibration/processing_tasks/calibration.py
@@ -44,13 +44,11 @@ def apply_gaintable(
     :return: Input Visibility with gains applied.
     """
     if vis.vis.ndim != gt.gain.ndim - 1:
-        raise ValueError("apply_gaintable: incompatible shapes")
+        raise ValueError("incompatible shapes")
     if vis.vis.shape[-1] != gt.gain.shape[-1] * gt.gain.shape[-1]:
-        raise ValueError("apply_gaintable: incompatible pol axis")
+        raise ValueError("incompatible pol axis")
     if inverse and gt.gain.shape[-1] != gt.gain.shape[-2]:
-        raise ValueError(
-            "apply_gaintable: gain inversion requires square matrices"
-        )
+        raise ValueError("gain inversion requires square matrices")
 
     shape = vis.vis.shape
 
@@ -59,9 +57,7 @@ def apply_gaintable(
 
     # need to know which dim has the antennas, so force the structure
     if vis.vis.ndim != 4 or vis.vis.shape[-1] != 4:
-        raise ValueError(
-            "apply_gaintable: expecting ska-sdp-datamodels xarrays"
-        )
+        raise ValueError("expecting ska-sdp-datamodels datasets")
 
     if inverse:
         # use pinv rather than inv to catch singular values
@@ -82,33 +78,76 @@ def apply_gaintable(
 def solve_bandpass(
     vis: xarray.Dataset,
     modelvis: xarray.Dataset,
-    gaintable: xarray.Dataset,
-    refant: Optional[int] = None,
+    gain_table: xarray.Dataset,
+    refant: int = 0,
 ) -> xarray.Dataset:
     """Determine bandpass calibration Jones matrices.
 
+    The spectral axes need to be consistent.
+    If gain_table.frequency==vis.frequency: solve with jones_type="B".
+    If gain_table.frequency ~ mean(vis.frequency): solve with jones_type="G".
+    Otherwise: raise a ValueError.
+
+    If gain_table.frequency has multiple channels but fewer than vis, it would
+    be possible to check that everything is aligned and loop over each output
+    channel separately. However, for now this is not supported. If bandpass
+    calibration with lower spectral resolution is required, call this function
+    separately for sub-bands of the desired width and a single output channel
+    each (i.e. for each sub-band call create_gaintable_from_visibility with
+    jones_type="G").
+
     :param vis: Visibility dataset with the unknown corruptions.
     :param modelvis: Visibility model dataset to solve against.
-    :param gaintable: GainTable dataset containing initial bandpass solutions.
-    :return: Update GainTable dataset.
+    :param gain_table: GainTable dataset containing bandpass solutions to be
+        updated.
+    :param refant: Reference antenna (defaults to 0).
+    :return: Updated GainTable dataset.
     """
-    logger.info("solving bandpass")
+    logger.debug("solving bandpass")
 
+    # Check inputs
+    if refant is not None:
+        if refant < 0 or refant >= len(gain_table.antenna):
+            raise ValueError(f"invalid refant: {refant}")
+
+    # Check spectral axes
+    if gain_table.frequency.equals(vis.frequency):
+        jones_type = "B"
+    elif len(gain_table.frequency) == 1:
+        jones_type = "G"
+        if gain_table.frequency.data[0] != np.mean(vis.frequency.data):
+            raise ValueError("Single-channel output is at the wrong frequency")
+    else:
+        raise ValueError("Only supports single-channel or all-channel output")
+
+    # Todo: Set vis flags from a user-defined list of known bad antennas?
+
+    # Initial unpolarised calibration?
     timeslice = vis.time.data.max() - vis.time.data.min()
 
-    solve_gaintable(
+    gain_table = solve_gaintable(
         vis=vis,
         modelvis=modelvis,
-        gain_table=gaintable,
+        gain_table=gain_table,
         solver="gain_substitution",
         phase_only=False,
         niter=200,
         tol=1e-06,
         crosspol=False,
         normalise_gains=None,
-        jones_type="B",
+        jones_type=jones_type,
         timeslice=timeslice,
         refant=refant,
     )
 
-    return gaintable
+    # Todo: Check for gain outliers and set flags? RFI or no signal...
+
+    # Todo: Further polarised calibration?
+
+    # Todo: Smoothing? Interpolation over missing channels?
+    #  - Careful of sub-band edge effects, but ok if just filling in the gaps.
+    #  - Best to leave smoothing until post-processing of all channels?
+
+    # Todo: Return lists of bad antennas and bad channels?
+
+    return gain_table
