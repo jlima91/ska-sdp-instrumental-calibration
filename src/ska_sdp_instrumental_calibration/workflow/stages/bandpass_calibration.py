5ff57edef0eb148bc32a52fddd0126ff14e6ae76
Justin Jose <justinj@thoughtworks.com>
2025-04-22 17:12:23 +0530

    DHR-221 Patch issue of delayed run solver in bandpass_callibration

--- src/ska_sdp_instrumental_calibration/workflow/stages/bandpass_calibration.py
+++ src/ska_sdp_instrumental_calibration/workflow/stages/bandpass_calibration.py
@@ -59,11 +59,20 @@ def bandpass_calibration_stage(upstream_output, run_solver_config, flagging):
     """
     # [TODO] if predict_vis stage is not run, obtain modelvis from data.
     modelvis = upstream_output.modelvis
+    initialtable = upstream_output.gaintable
+    vis = upstream_output.vis
 
-    gaintable = dask.delayed(run_solver)(
-        vis=upstream_output.vis,
+    # [TODO] Remove this section once model_rotations returns xarray
+    run_solver_func = (
+        dask.delayed(run_solver)
+        if hasattr(initialtable, "dask")
+        else run_solver
+    )
+
+    gaintable = run_solver_func(
+        vis=vis,
         modelvis=modelvis,
-        gaintable=upstream_output.gaintable,
+        gaintable=initialtable,
         solver=run_solver_config["solver"],
         niter=run_solver_config["niter"],
         refant=run_solver_config["refant"],
