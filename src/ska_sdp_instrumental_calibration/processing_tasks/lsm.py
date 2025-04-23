YAB-38: bug fix -- convert comp.beam_pa to radians in deconvolve_gaussian in the circular Gaussian case

src/ska_sdp_instrumental_calibration/processing_tasks/lsm.py
@@ -86,7 +86,7 @@ def generate_lsm_from_gleamegc(
     See data class :class:`~Component` and the gleamegc ReadMe file for more
     information on columns extracted from the catalogue.
 
-    :param cat: Catalogue filename. Must follow the gleamegc.dat format.
+    :param gleamfile: Catalogue filename. Must follow the gleamegc.dat format.
     :param phasecentre: astropy SkyCoord for the centre of the sky model.
     :param fov: Field of view in degrees. Default is 5.
     :param flux_limit: minimum flux density in Jy. Default is 0.
@@ -271,7 +271,7 @@ def deconvolve_gaussian(comp: Component) -> tuple[float]:
         # Circular Gaussian case
         smaj = np.sqrt(fmajsq - bminsq)
         smin = np.sqrt(fmajsq - bmajsq)
-        psmaj = np.pi / 2.0 + comp.beam_pa
+        psmaj = np.pi / 2.0 + comp.beam_pa * np.pi / 180.0
 
     else:
         # General case

tests/unit/test_lsm.py
@@ -337,6 +337,9 @@ def test_deconvolve_circular_gaussian():
     Given a component, deconvolve MWA synthesised beam.
     if the gaussian is circular, then handle it appropriately.
     """
+    beam_major = 200
+    beam_minor = 150
+    beam_pa = 20
     component = Component(
         name="J12345",
         RAdeg=260,
@@ -347,12 +350,12 @@ def test_deconvolve_circular_gaussian():
         major=250,
         minor=250,
         pa=-4,
-        beam_major=200,
-        beam_minor=150,
-        beam_pa=20,
+        beam_major=beam_major,
+        beam_minor=beam_minor,
+        beam_pa=beam_pa,
     )
 
     actual_params = np.array(deconvolve_gaussian(component))
-    expectd_params = np.array((200.0, 150.0, 1235.9155902616465))
+    expectd_params = np.array((beam_major, beam_minor, 90 + beam_pa))
 
     np.testing.assert_allclose(expectd_params, actual_params)