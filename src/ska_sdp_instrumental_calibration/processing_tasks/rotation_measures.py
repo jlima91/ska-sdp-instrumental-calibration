8ac31aa64facf4d8176351d64def2cd12148ea77
Justin Jose <justinj@thoughtworks.com>
2025-05-13 11:28:05 +0530

    DHR-191: Refactor rotation measure to be lazy computed
    Co-authored-by: Maneesh Sutar <msutar@thoughtworks.com>

---
+++ src/ska_sdp_instrumental_calibration/processing_tasks/rotation_measures.py
@@ -0,0 +1,154 @@
+"""Post-calibration fits."""
+
+__all__ = ["model_rotations"]
+
+from pathlib import Path
+
+import dask
+import dask.array as da
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+import xarray as xr
+from astropy import constants as const
+
+from ska_sdp_instrumental_calibration.logger import setup_logger
+
+logger = setup_logger("processing_tasks.post_processing")
+
+
+def _plot_rm(
+    rm_peek: np.float64,
+    rm_spec: npt.NDArray[np.complex],
+    rm_vals: npt.NDArray[np.float64],
+    title: str,
+    plot_path: Path | str,
+):
+    xlim = 3 * np.max(np.abs(rm_peek))
+
+    plt.figure()
+
+    ax = plt.subplot(111)
+    ax.set_title(title)
+    ax.set_xlabel("RM (rad / m^2)")
+    ax.plot(rm_vals, np.abs(rm_spec), "b", label="abs")
+    ax.plot(rm_vals, np.real(rm_spec), "c", label="re")
+    ax.plot(rm_vals, np.imag(rm_spec), "m", label="im")
+    ax.plot(rm_peek * np.ones(2), ax.get_ylim(), "b--")
+    ax.set_xlim((-xlim, xlim))
+    ax.grid()
+    ax.legend()
+
+    plt.savefig(plot_path)
+
+
+@dask.delayed
+def _get_rm_peeks(
+    rm_spec: npt.NDArray[np.complex],
+    rm_vals: npt.NDArray[np.float64],
+    peak_threshold: float,
+    nstations: int,
+    plot_path_prefix: str,
+    plot_sample: bool = False,
+):
+    stn = nstations - 1
+    rm_peeks = np.zeros(nstations)
+
+    update_indexes = np.where(np.max(np.abs(rm_spec), axis=1) > peak_threshold)
+    rm_peeks[update_indexes] = rm_vals[
+        np.argmax(np.abs(rm_spec[update_indexes]), axis=1)
+    ]
+
+    if plot_sample:
+        _plot_rm(
+            rm_peeks[stn],
+            rm_spec[stn],
+            rm_vals,
+            f"RM spectrum for station {stn}",
+            f"{plot_path_prefix}_{stn}.png",
+        )
+
+    return rm_peeks
+
+
+def model_rotations(
+    gaintable: xr.Dataset,
+    peak_threshold: float = 0.5,
+    plot_sample: bool = False,
+    plot_path_prefix: str = "./",
+    ref: int = 0,
+) -> xr.Dataset:
+    """Fit a rotation measure for each station Jones matrix.
+
+    For each station, the Jones matrix for each channel is used to
+    operate on a unit vector. The result is expressed as a complex
+    number, and the spectrum of complex numbers is the Fourier
+    transformed with respect to wavelength squared. The peaks of this
+    transformed spectrum is taken as the rotation measure for the
+    station, and used to initialise a new gaintable.
+
+    :param gaintable: GainTable dataset to be to modelled.
+    :param peak_threshold: Height of peak in the RM spectrum required for a
+        rotation detection.
+    :param plot_sample: Whether or not to plot a sample RM spectrum.
+    "return": new GainTable dataset with model rotations.
+    """
+
+    oversample = 5
+    nstations = len(gaintable.antenna)
+    nfreq = len(gaintable.frequency)
+
+    lambda_sq = (
+        const.c.value / gaintable.frequency  # pylint: disable=no-member
+    ) ** 2
+
+    rm_res = 1 / oversample / (np.max(lambda_sq) - np.min(lambda_sq))
+    rm_max = 1 / (lambda_sq[-2] - lambda_sq[-1])
+    rm_vals = da.arange(-rm_max, rm_max, rm_res)
+
+    uvec = np.array([1, 0])
+
+    invN = 1 / len(gaintable.frequency)
+    invR = 1 / np.sqrt(2)
+
+    reljones = da.einsum(
+        "sfpx,fqx->sfpq",
+        gaintable.gain[0, :],
+        gaintable.gain[0, ref].conj(),
+    )
+    reljones /= invR * da.linalg.norm(reljones, axis=(2, 3), keepdims=True)
+
+    rvec = da.einsum("sfpq,q->sfp", reljones, uvec)
+    f_spec = rvec[:, :, 0] + 1j * rvec[:, :, 1]
+
+    rm_spec = invN * da.einsum(
+        "rf,sf->sr",
+        np.exp(np.einsum("i,j->ij", -1j * rm_vals, lambda_sq)),
+        f_spec,
+    )
+
+    rm_peeks = da.from_delayed(
+        _get_rm_peeks(
+            rm_spec,
+            rm_vals,
+            peak_threshold,
+            nstations,
+            plot_sample,
+            plot_path_prefix,
+        ),
+        (nstations,),
+        rm_vals.dtype,
+    )
+
+    peek_diff = rm_peeks - rm_peeks[ref]
+    d_pa = lambda_sq.data * peek_diff.reshape(nstations, -1)
+
+    updated_gain = da.stack(
+        (np.cos(d_pa), -np.sin(d_pa), np.sin(d_pa), np.cos(d_pa)),
+        axis=2,
+    ).reshape(1, nstations, nfreq, 2, 2)
+
+    updated_gain_xr = gaintable.gain.copy()
+    updated_gain_xr.data = updated_gain
+
+    return gaintable.assign({"gain": updated_gain_xr})
