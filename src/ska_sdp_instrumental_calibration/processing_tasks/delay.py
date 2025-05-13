b4bf138a2a38cb62260078922099b51718e80092
Maneesh Sutar <maneesh.sutar@thoughtworks.com>
2025-05-13 17:44:43 +0530

    DHR-191: Make delay calibration dask complient
    Co-authored-by: Justin Jose <justinj@thoughtworks.com>

--- src/ska_sdp_instrumental_calibration/processing_tasks/delay.py
+++ src/ska_sdp_instrumental_calibration/processing_tasks/delay.py
@@ -1,3 +1,5 @@
+import dask
+import dask.array as da
 import numpy as np
 import xarray as xr
 
@@ -17,33 +19,42 @@ def apply_delay(gaintable: xr.Dataset, oversample) -> xr.Dataset:
         gaintable: xr.Dataset
             Gaintable with updated gains
     """
+    nstations = len(gaintable.antenna)
+    new_gain_data = gaintable.gain.data.copy()
 
-    Xgain = gaintable.gain.data[0, :, :, 0, 0]
-    Ygain = gaintable.gain.data[0, :, :, 1, 1]
+    Xgain = new_gain_data[0, :, :, 0, 0]
+    Ygain = new_gain_data[0, :, :, 1, 1]
 
     Xdelay_coarse = coarse_delay(gaintable.frequency, Xgain, oversample)
     Xdelay, Xoffset = update_delay(
-        gaintable, np.zeros(20, dtype=float), Xdelay_coarse, pol=0
+        gaintable,
+        da.zeros(nstations, dtype=float),
+        Xdelay_coarse,
+        pol=0,
     )
 
     Ydelay_coarse = coarse_delay(gaintable.frequency, Ygain, oversample)
     Ydelay, Yoffset = update_delay(
-        gaintable, np.zeros(20, dtype=float), Ydelay_coarse, pol=1
+        gaintable,
+        da.zeros(nstations, dtype=float),
+        Ydelay_coarse,
+        pol=1,
     )
-    Xgain = calculate_gain_rot(
+
+    new_gain_data[0, :, :, 0, 0] = calculate_gain_rot(
         Xgain, Xdelay, Xoffset, gaintable.frequency.data.reshape(1, -1)
     )
-    Ygain = calculate_gain_rot(
+    new_gain_data[0, :, :, 1, 1] = calculate_gain_rot(
         Ygain, Ydelay, Yoffset, gaintable.frequency.data.reshape(1, -1)
     )
 
-    gaintable["gain"].data[0, :, :, 0, 0] = Xgain
-    gaintable["gain"].data[0, :, :, 1, 1] = Ygain
+    new_gain = gaintable.gain.copy()
+    new_gain.data = new_gain_data
 
-    return gaintable
+    return gaintable.assign({"gain": new_gain}).chunk(gaintable.chunks)
 
 
-def update_delay(gaintable, offset, delay, pol):
+def update_delay(gaintable, _offset, delay, pol):
     """
     Updates the delay to the gains
 
@@ -51,7 +62,7 @@ def update_delay(gaintable, offset, delay, pol):
     -----------
         gaintable: xr.Dataset
             Gaintable
-        offset: np.array
+        _offset: np.array
             Calculated offset per station
         delay: np.array
             Calculated delays per station
@@ -66,23 +77,34 @@ def update_delay(gaintable, offset, delay, pol):
     gains = gaintable.gain.data[0, :, :, pol, pol]
     wgt = gaintable.weight.data[0, :, :, pol, pol]
 
-    gains_rot = calculate_gain_rot(gains, delay, offset, freq)
+    gains_rot = calculate_gain_rot(gains, delay, _offset, freq)
 
-    cycles = np.unwrap(np.angle(gains_rot)) / (2 * np.pi)
+    cycles = da.from_delayed(
+        calculate_cycles(gains_rot), gains.shape, gains_rot.real.dtype
+    )
 
     denom = np.sum(wgt, axis=1) * np.sum(wgt * freq * freq, axis=1) - np.sum(
         wgt * freq, axis=1
     ) * np.sum(wgt * freq, axis=1)
-    offset += (
-        np.sum(wgt * freq * freq, axis=1) * np.sum(wgt * cycles, axis=1)
-        - np.sum(wgt * freq, axis=1) * np.sum(wgt * freq * cycles, axis=1)
-    ) / denom
+    offset = (
+        _offset
+        + (
+            np.sum(wgt * freq * freq, axis=1) * np.sum(wgt * cycles, axis=1)
+            - np.sum(wgt * freq, axis=1) * np.sum(wgt * freq * cycles, axis=1)
+        )
+        / denom
+    )
 
     updated_delay = (
         np.sum(wgt, axis=1) * np.sum(wgt * cycles * freq, axis=1)
         - np.sum(wgt * cycles, axis=1) * np.sum(wgt * freq, axis=1)
     ) / denom
     return delay + updated_delay, offset
+
+
+@dask.delayed
+def calculate_cycles(gains_rot):
+    return np.unwrap(np.angle(gains_rot)) / (2 * np.pi)
 
 
 def coarse_delay(frequency, gains, oversample):
@@ -105,17 +127,17 @@ def coarse_delay(frequency, gains, oversample):
     """
     nstations, nchan = gains.shape
     N = oversample * nchan
-    padded_gains = np.zeros((nstations, N), "complex")
+    padded_gains = da.zeros((nstations, N), "complex")
     gain_start_index = N // 2 - nchan // 2
     gain_stop_index = N // 2 + nchan // 2
     padded_gains[:, gain_start_index:gain_stop_index] = gains
-    delay_spec = np.fft.fftshift(np.fft.fft(padded_gains, axis=1), axes=(1,))
+    delay_spec = da.fft.fftshift(da.fft.fft(padded_gains, axis=1), axes=(1,))
 
     delay = ((1 / oversample) / (frequency[-1] - frequency[0]).data) * (
-        -N // 2 + np.arange(N)
+        -N // 2 + da.arange(N)
     )
 
-    return delay[np.abs(delay_spec).argmax(axis=1)]
+    return delay[da.abs(delay_spec).argmax(axis=1)]
 
 
 def calculate_gain_rot(gain, delay, offset, freq):
--- tests/unit/test_delay.py
+++ tests/unit/test_delay.py
@@ -1,12 +1,11 @@
+# pylint: disable = too-many-function-args
 import numpy as np
 import xarray as xr
-from mock import MagicMock, patch
 
 from ska_sdp_instrumental_calibration.processing_tasks.delay import (
     apply_delay,
     calculate_gain_rot,
     coarse_delay,
-    update_delay,
 )
 
 
@@ -22,33 +21,6 @@ def test_should_calculate_coarse_delay():
     actual = coarse_delay(frequency, gains, oversample)
 
     assert np.allclose(expected, actual, atol=1e-11)
-
-
-@patch("ska_sdp_instrumental_calibration.processing_tasks.delay.np")
-def test_coarse_delay(np_mock):
-    freq_mock = MagicMock()
-    gain_mock = MagicMock(name="gain_mock")
-    gain_mock.shape = (20, 64)
-    oversample = 16
-    zeros_mock = MagicMock(name="zeros_mock")
-    fft_value_mock = MagicMock()
-    fft_shift_value_mock = MagicMock()
-    abs_value_mock = MagicMock()
-    arange_value_mock = MagicMock()
-
-    np_mock.zeros.return_value = zeros_mock
-    np_mock.fft.fft.return_value = fft_value_mock
-    np_mock.fft.fftshift.return_value = fft_shift_value_mock
-    np_mock.arange.return_value = arange_value_mock
-    np_mock.abs.return_value = abs_value_mock
-
-    coarse_delay(freq_mock, gain_mock, oversample)
-
-    np_mock.zeros.assert_called_once_with((20, 1024), "complex")
-    np_mock.fft.fft.assert_called_once_with(zeros_mock, axis=1)
-    np_mock.fft.fftshift.assert_called_once_with(fft_value_mock, axes=(1,))
-    np_mock.arange.assert_called_once_with(1024)
-    np_mock.abs.assert_called_once_with(fft_shift_value_mock)
 
 
 def test_calculate_gain_rotation():
@@ -77,76 +49,84 @@ def test_calculate_gain_rotation():
     assert np.allclose(actual, expected)
 
 
-@patch("ska_sdp_instrumental_calibration.processing_tasks.delay.np")
-@patch(
-    "ska_sdp_instrumental_calibration.processing_tasks.delay"
-    ".calculate_gain_rot"
-)
-def test_update_delay(cal_gain_rot_mock, np_mock):
+def test_calculate_apply_delay():
+    coords = {
+        "antenna": ["antenna1", "antenna2"],
+        "frequency": np.linspace(1.0010e8, 1.0019e8, 4, dtype=np.float64),
+    }
 
-    freq_mock = MagicMock(name="freq_mock")
-    gains_mock = MagicMock(name="gains_mock")
-    weights_mock = MagicMock(name="weights_mock")
-    gains_rot_mock = MagicMock(name="gains_rot_mock")
-    cycles_mock = MagicMock(name="cycles_mock")
-    sum_result_mock = MagicMock(name="sum_result_mock")
-    offset_mock = MagicMock(name="offset_mock")
-    delay_mock = MagicMock(name="delay_mock")
-    gaintable = MagicMock(name="gaintable")
-
-    np_mock.unwrap.return_value = cycles_mock
-    np_mock.angle.return_value = cycles_mock
-    np_mock.sum.return_value = sum_result_mock
-    gaintable.frequency.data.reshape.return_value = freq_mock
-    gaintable.gain.data.__getitem__.return_value = gains_mock
-    gaintable.weight.data.__getitem__.return_value = weights_mock
-    cal_gain_rot_mock.return_value = gains_rot_mock
-
-    update_delay(gaintable, offset_mock, delay_mock, 0)
-
-    cal_gain_rot_mock.assert_called_once_with(
-        gains_mock, delay_mock, offset_mock, freq_mock
+    # gain_shape = [ntimes, nants, nfrequency, nrec, nrec]
+    gains = xr.DataArray(
+        np.array(
+            [
+                np.cos(np.pi / 3) + 1j * np.sin(np.pi / 3),
+                np.cos(2 * np.pi / 3) + 1j * np.sin(2 * np.pi / 3),
+            ]
+            * 16,
+            dtype=np.complex128,
+        ).reshape(1, 2, 4, 2, 2),
+        dims=["time", "antenna", "frequency", "rec1", "rec2"],
     )
 
-
-@patch("ska_sdp_instrumental_calibration.processing_tasks.delay.np")
-@patch("ska_sdp_instrumental_calibration.processing_tasks.delay.update_delay")
-@patch("ska_sdp_instrumental_calibration.processing_tasks.delay.coarse_delay")
-@patch(
-    "ska_sdp_instrumental_calibration.processing_tasks.delay"
-    ".calculate_gain_rot"
-)
-def test_apply_delay(
-    cal_gain_rot_mock, coarse_delay_mock, update_delay_mock, np_mock
-):
-    gains_mock = MagicMock(name="gains_mock")
-    weights_mock = MagicMock(name="weights_mock")
-    cycles_mock = MagicMock(name="cycles_mock")
-    sum_result_mock = MagicMock(name="sum_result_mock")
-    xgain_mock = MagicMock(name="xgain_mock")
-    xoffset_mock = MagicMock(name="xoffset_mock")
-    xdelay_coarse_mock = MagicMock(name="xdelay_coarse_mock")
-    reshape_mock = MagicMock(name="reshape_mock")
-    gaintable = MagicMock(name="gaintable")
-    np_zeros_mock = MagicMock(name="np_zeros_mock")
-
-    np_mock.unwrap.return_value = cycles_mock
-    np_mock.angle.return_value = cycles_mock
-    np_mock.sum.return_value = sum_result_mock
-    update_delay_mock.return_value = [xgain_mock, xoffset_mock]
-    coarse_delay_mock.return_value = xdelay_coarse_mock
-    gaintable.frequency.data.__getitem__.side_effect = [xgain_mock, xgain_mock]
-    gaintable.frequency.data.reshape.return_value = reshape_mock
-    gaintable.gain.data.__getitem__.return_value = gains_mock
-    gaintable.weight.data.__getitem__.return_value = weights_mock
-    np_mock.zeros.return_value = np_zeros_mock
-
-    apply_delay(gaintable, oversample=16)
-
-    coarse_delay_mock.assert_called_with(gaintable.frequency, gains_mock, 16)
-    update_delay_mock.assert_called_with(
-        gaintable, np_zeros_mock, coarse_delay_mock.return_value, pol=1
+    weights = xr.DataArray(
+        np.ones(32, dtype=np.float64).reshape(1, 2, 4, 2, 2),
+        dims=["time", "antenna", "frequency", "rec1", "rec2"],
     )
-    cal_gain_rot_mock.assert_called_with(
-        gains_mock, xgain_mock, xoffset_mock, reshape_mock
+
+    gaintable = xr.Dataset(
+        {
+            "gain": gains,
+            "weight": weights,
+        },
+        coords=coords,
+    )
+
+    actual_gaintable = apply_delay(gaintable, 4)
+
+    expected_gain = np.array(
+        [
+            [
+                [
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                ],
+                [
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                    [
+                        [1.0 + 1.48741681e-17j, -0.5 + 8.66025404e-01j],
+                        [0.5 + 8.66025404e-01j, 1.0 - 2.57628149e-17j],
+                    ],
+                ],
+            ]
+        ]
+    )
+
+    np.testing.assert_allclose(
+        np.angle(actual_gaintable.gain.data, deg=True),
+        np.angle(expected_gain, deg=True),
     )
