835d1a2cc50101496db84fdd66518dac4b24689e
Justin Jose <justinj@thoughtworks.com>
2025-05-21 19:36:20 +0530

    DHR-186: Export gain solutions for smoothing

--- tests/unit/ska_sdp_instrumental_calibration/workflow/test_utils.py
+++ tests/unit/ska_sdp_instrumental_calibration/workflow/test_utils.py
@@ -536,7 +536,6 @@ def test_should_plot_when_stations_are_less_than_subplot_capacity(
     )
 
 
-
 def test_should_convert_earth_centric_coordinates_to_geodetic():
     x = np.array([-5133977.79947732, -5133977.79947732])
     y = np.array([10168886.79974105, -10168886.79974105])
@@ -547,4 +546,3 @@ def test_should_convert_earth_centric_coordinates_to_geodetic():
     np.testing.assert_allclose(lat, np.array([-26.753052, -26.753052]))
     np.testing.assert_allclose(lng, np.array([116.787894, 243.212106]))
     np.testing.assert_allclose(alt, np.array([6374502.632896, 6374502.632896]))
-
