9543119ebdfb652d5415f462bfaf14b2877ef3c1
Maneesh Sutar <maneesh.sutar@thoughtworks.com>
2025-08-10 12:35:10 +0530

    REL-2252: Bump version to 0.4.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -32,8 +32,8 @@ def setup(app):
 project = "SKA SDP Instrumental Calibration Pipeline"
 copyright = "2025, SKA Organization"
 author = "Team YABBY, Team DHRUVA"
-release = "0.3.4"
-version = "0.3.4"
+release = "0.4.0"
+version = "0.4.0"
 
 # -- General configuration ------------------------------------------------
 
