8feb6a9a3155a36d86ff675640412f0384df94de
Justin Jose <justinj@thoughtworks.com>
2025-12-01 10:21:36 +0530

    DHR-379: Import solvers in init to register with factory

--- src/ska_sdp_instrumental_calibration/numpy_processors/solvers/__init__.py
+++ src/ska_sdp_instrumental_calibration/numpy_processors/solvers/__init__.py
@@ -1,4 +1,17 @@
+from .alternative_solvers import (
+    JonesSubtitution,
+    NormalEquation,
+    NormalEquationsPreSum,
+)
+from .gain_substitution_solver import GainSubstitution
 from .solver import Solver
 from .solvers_factory import SolverFactory
 
-__all__ = ["SolverFactory", "Solver"]
+__all__ = [
+    "SolverFactory",
+    "Solver",
+    "JonesSubtitution",
+    "NormalEquation",
+    "NormalEquationsPreSum",
+    "GainSubstitution",
+]
